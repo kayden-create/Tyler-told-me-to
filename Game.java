22c8b5c4f9b36e95072a1aa21efd0730a74508ea
kayden-create <kaydenpschaefer@gmial.com>
2025-07-24 07:59:57 -0500

    learning still

--- Game.java
+++ Game.java
@@ -1,5 +1,12 @@
-class Game{
+import java.util.Scanner;
+
+public class Game {
     public static void main(String [] args){
- System.out.println("tyler is annoying!");       
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello there!!");
+        System.out.print("what is you opinion on Tyler?");
+        String opinion = scan.nextLine(); 
+        System.out.println("I agree Tyler is  " + opinion + "!!");
+        scan.close();
+        }
     }
-}
