e1177f071f687edc5c71f92d7c0780ff967747a8
kayden-create <kaydenpschaefer@gmial.com>
2025-08-02 11:12:10 -0500

    players and exit

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
@@ -15,14 +17,95 @@ public class Board extends JFrame {
         setTitle("Kayden's Chess Board");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());;
+        JButton back = new JButton("back to menu");
+        JLabel one = new JLabel("1");
+        JLabel two = new JLabel("2");
+        JLabel three = new JLabel("3");
+        JLabel four = new JLabel("4");
+        JLabel five = new JLabel("5");
+        JLabel six = new JLabel("6");
+        JLabel seven = new JLabel("7");
+        JLabel eight = new JLabel("8");
+        JLabel a = new JLabel("a");
+        JLabel b = new JLabel("b");
+        JLabel c = new JLabel("c");
+        JLabel d = new JLabel("d");
+        JLabel ee = new JLabel("e");
+        JLabel f = new JLabel("f");
+        JLabel g = new JLabel("g");
+        JLabel h = new JLabel("h");
+        add(one);
+        add(two);
+        add(three);
+        add(four);
+        add(five);
+        add(six);
+        add(seven);
+        add(eight);
+        add(a);
+        add(b);
+        add(c);
+        add(d);
+        add(ee);
+        add(f);
+        add(g);
+        add(h);
+        a.setBounds(167, 613, 50, 50);
+        b.setBounds(237, 613, 50, 50);
+        c.setBounds(307, 613, 50, 50);
+        d.setBounds(377, 613, 50, 50);
+        ee.setBounds(447, 613, 50, 50);
+        f.setBounds(517, 613, 50, 50);
+        g.setBounds(587, 613, 50, 50);
+        h.setBounds(657, 613, 50, 50);
+        a.setForeground(new Color(200, 200, 200));
+        b.setForeground(new Color(0, 0, 0));
+        c.setForeground(new Color(200, 200, 200));
+        d.setForeground(new Color(0, 0, 0));
+        ee.setForeground(new Color(200, 200, 200));
+        f.setForeground(new Color(0, 0, 0));
+        g.setForeground(new Color(200, 200, 200));
+        h.setForeground(new Color(0, 0, 0));
+        eight.setBounds(115, 97, 50, 50);
+        seven.setBounds(115, 165, 50, 50);
+        six.setBounds(115, 232, 50, 50);
+        five.setBounds(115, 300, 50, 50);
+        four.setBounds(115, 367, 50, 50);
+        three.setBounds(115, 434, 50, 50);
+        two.setBounds(115, 501, 50, 50);
+        one.setBounds(115, 568, 50, 50);
+        one.setForeground(new Color(200, 200, 200));
+        three.setForeground(new Color(200, 200, 200));
+        five.setForeground(new Color(200, 200, 200));
+        seven.setForeground(new Color(200, 200, 200));
+        two.setForeground(new Color(0, 0, 0));
+        four.setForeground(new Color(0, 0, 0));
+        six.setForeground(new Color(0, 0, 0));
+        eight.setForeground(new Color(0, 0, 0));
+        JLabel name = new JLabel("Player 1");
+        JLabel name2 = new JLabel("Player 2");
         JPanel myboard = new JPanel();
-                JPanel player1 = new JPanel();
+        JPanel player1 = new JPanel();
         JPanel player2 = new JPanel();
         JPanel left = new JPanel();
         JPanel right = new JPanel();
+        name.setForeground(new Color(0, 0, 0));
+        name2.setForeground(new Color(0, 0, 0));
+        name.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 16));
+        name2.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 16));
+        player1.add(name);
+        player2.add(name2);
+
         myboard.setLayout(new GridLayout(8, 8));
         squares = new JPanel();
-       
+       left.add(back);
+       myboard.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        back.addActionListener(e -> {
+           Menu menu = new Menu();
+           menu.display();
+              this.dispose(); // Close the board window
+        });
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 squares = new JPanel();
@@ -35,22 +118,23 @@ public class Board extends JFrame {
                 myboard.add(squares);
             }
         }
+    
         player1.setPreferredSize(new Dimension(100,100));
         player2.setPreferredSize(new Dimension(100,100));
         left.setPreferredSize(new Dimension(100,100));
         right.setPreferredSize(new Dimension(100,100));
-        player1.setBackground(new Color(200, 200, 200));
-        player2.setBackground(new Color(200, 200, 200));
+        player1.setBackground(new Color(0, 0, 200));
+        player2.setBackground(new Color(200, 0, 0));
         left.setBackground(new Color(200, 200, 200));
         right.setBackground(new Color(200, 200, 200));
         myboard.setBounds(100,100, 600, 600);
         this.add(myboard, BorderLayout.CENTER);
-        add(player1, BorderLayout.NORTH);
-        add(player2, BorderLayout.SOUTH);   
+        add(player1, BorderLayout.SOUTH);
+        add(player2, BorderLayout.NORTH);   
         add(left, BorderLayout.WEST);
         add(right, BorderLayout.EAST);
         setSize(800, 800);
-        setResizable(true); 
+        setResizable(false); 
         setVisible(false); 
     }
 }
--- Menu.java
+++ Menu.java
@@ -10,24 +10,28 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Menu extends JFrame{
-    JButton button, button2;
+    JButton button, button2, button3;
     
     public Menu() {
         JPanel panel = new JPanel();
         panel.setLayout(null);
-        
+        button = new JButton("Start Chess Game");
+        button2 = new JButton("Start Checkers Game");
+        button3 = new JButton("Exit Game");
         panel.setBackground(new Color(200, 200, 200));
         panel.setBounds(150, 100, 500, 600);
-        JLabel label = new JLabel("Welcome to the Chess Game!"); 
+        JLabel label = new JLabel("Welcome to the Kayden's Game!"); 
         label.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 16)); 
         label.setForeground(new Color(0, 0, 0));
         label.setLayout(null);
         label.setBounds(140, 100, 250, 50);
         panel.add(label);
         panel.setBorder(BorderFactory.createLoweredBevelBorder());
+        add(button);
+        add(button2);
+        add(button3);
         add(panel);
 
-        button = new JButton("Start Chess Game");
         button.setBounds(300, 250, 200, 50);
         button.setBackground(new Color(100, 150, 255));
         button.setForeground(new Color(0, 0, 0));
@@ -39,27 +43,32 @@ public class Menu extends JFrame{
             board.setVisible(true);
             this.dispose(); // Close the start window
         });
-
-        button2 = new JButton("Whatever man");
         button2.setBounds(300, 350, 200, 50);
         button2.setBackground(new Color(100, 150, 255));
         button2.setForeground(new Color(0, 0, 0));
         button2.setBorder(BorderFactory.createEtchedBorder());
         button2.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 16));
         button2.addActionListener(e -> {
-            System.out.println("Chess Game Started");
+            System.out.println("Checkers Game Started");
             Board board = new Board();
             board.setVisible(true);
             this.dispose(); // Close the start window
         });
-
+        button3.setBounds(300, 450, 200, 50);
+        button3.setBackground(new Color(100, 150, 255));
+        button3.setForeground(new Color(0, 0, 0));
+        button3.setBorder(BorderFactory.createEtchedBorder());
+        button3.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 16));
+        button3.addActionListener(e -> {
+            System.out.println("game exited");
+            System.exit(0); // Exit the game
+        });
         setTitle("My Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setSize(800, 800);
         setResizable(false);
-        add(button);
-        add(button2);
+        
         getContentPane().setBackground(Color.darkGray);
         panel.revalidate();
         panel.repaint();
