dea05b1641493daca3e0619c28c0de520f967e18
TSSchaef <tschaeferstorm@gmail.com>
2025-12-04 17:44:57 -0600

    Fixed OS issue for loading images and consolidate Pawn into single class

--- Piece.java
+++ Piece.java
@@ -1,8 +1,18 @@
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+
+enum PieceType {
+    PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING
+}
+
+enum PieceColor {
+    WHITE, BLACK
+}
+
 public interface Piece {
     JLabel drawPiece();
     void canMove(/*Put in target square to move to */);
     void setBounds(int x, int y, int width, int height);
-    
+    PieceType getType();
+    PieceColor getColor();
 }
